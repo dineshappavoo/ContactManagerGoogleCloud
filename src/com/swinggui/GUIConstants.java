ec3e16317cbf2e4bc7e4104e4598068dc153ce3c
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-05 02:22:51 -0500

    Contact manager gui design

---
+++ src/com/swinggui/GUIConstants.java
@@ -0,0 +1,21 @@
+
+package com.swinggui;
+
+/**
+ * 
+ */
+
+
+/**
+ * @author Dinesh Appavoo
+ *
+ */
+public class GUIConstants {
+
+	public static final int _XPOS_LEFT=250;
+	public static final int _XPOS_RIGHT=360;
+	public static final int _YPOS=100;
+	public static final String[] stateList={"Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"};
+	public static final String _FILE_NAME="/users/dany/downloads/contacts.txt";
+
+}

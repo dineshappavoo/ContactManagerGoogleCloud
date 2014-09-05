c1b3b94038f02844196d4d64d01a144b44030692
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-05 02:21:34 -0500

    Contact Manager Application developed in Java swings and Google Cloud Storage.

---
+++ src/com/swinggui/AlertUtil.java
@@ -0,0 +1,28 @@
+
+package com.swinggui;
+/**
+ * 
+ */
+
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Dany
+ *
+ */
+public class AlertUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void msgbox(String s){
+		   JOptionPane.showMessageDialog(null, s);
+		}
+
+}
